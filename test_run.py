09ad0e77bdb897c5c7434d7349dc273ebf06b9c1
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 10:32:33 +0100

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -21,20 +21,20 @@ def test_rename_columns():
     assert "sepal_length" in data_renamed.columns
 
 
-def test_get_sample(self):
+def test_get_sample():
     """
     Test
     """
     data = import_data()
     data_sample = get_sample(data)
-    self.assertEqual(len(data_sample), 50)
+    assert (len(data_sample), 50)
 
-def test_multiply_dataset(self):
+def test_multiply_dataset():
     """
     Test
     """
     data = import_data()
     data_sample = get_sample(data)
     data_multiply = multiply_dataset(data_sample)
-    self.assertEqual(len(data_multiply), len(data_sample) * 2, "Number of rows is not doubled.")
+    assert (len(data_multiply), len(data_sample) * 2, "Number of rows is not doubled.")
 
