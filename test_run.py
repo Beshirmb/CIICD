080741a47f5c78e08b1fd9936c465b8fcd2c0a9b
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 09:35:56 +0100

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -3,12 +3,14 @@ Tests
 """
 from run import import_data, rename_columns, get_sample, multiply_dataset
 
+
 def test_import_data():
     """
     Test
     """
     data = import_data()
     assert data.shape[0] > 0
+
 
 def test_rename_columns():
     """
@@ -18,13 +20,15 @@ def test_rename_columns():
     data_renamed = rename_columns(data)
     assert "sepal_length" in data_renamed.columns
 
+
 def test_get_sample():
     """
     Test
     """
     data = import_data()
     data_sample = get_sample(data)
-    
+
+
 def test_multiply_dataset():
     """
     Test
